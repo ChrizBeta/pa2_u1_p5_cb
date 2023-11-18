d529396751b950f86394572f2afcc56190170fd3
ChrizBeta <chrisalejo@hotmail.es>
2023-11-17 19:47:48 -0500

    taller7

--- src/main/java/com/uce/edu/Pa2U1P5CbApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5CbApplication.java
@@ -1,13 +1,23 @@
 package com.uce.edu;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+import com.uce.edu.repository.modelo.Materia;
+import com.uce.edu.service.IMateriaService;
 
 @SpringBootApplication
 public class Pa2U1P5CbApplication implements CommandLineRunner {
+
+	@Autowired
+	private IMateriaService iMateriaService;
+	
+	@Autowired
+	private Materia materia;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5CbApplication.class, args);
@@ -16,9 +26,34 @@ public class Pa2U1P5CbApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	
-	
+		//PONGO DATOS A LA MATERIA
+		this.materia.setCodigo("M1");
+		this.materia.setNombre("P.Avanzada");
+		this.materia.setNumerocreditos(10);
 		
-	
+		//REGISTRO O CREO MATERIA
+		this.iMateriaService.registrar(materia);
+		
+		System.out.println("REPORTE INICIO");
+		List<Materia> reporte = this.iMateriaService.buscarTodos();
+		for(Materia mat : reporte) {
+			System.out.println(mat);
+		}
+		System.out.println("REPORTE FIN");
+		
+		Materia mate = this.iMateriaService.buscar("M1");
+		System.out.println(mate);
+		
+		mate.setNumerocreditos(20);
+		this.iMateriaService.actualizar(mate);
+		mate = this.iMateriaService.buscar("M1");
+		System.out.println(mate);
+		
+		this.iMateriaService.eliminar("M1");
+		mate = this.iMateriaService.buscar("M1");
+		System.out.println(mate);
+		
+		
 		
 	}
 }
